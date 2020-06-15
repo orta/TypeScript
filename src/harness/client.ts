4825db1515d5651e8c8cd2922554e2fc1b083ec8
Orta Therox <orta.therox@gmail.com>
2020-06-15 16:42:22 -0400

    Adds the ability to test modern semantic classification via strings instead of numbers

--- src/harness/client.ts
+++ src/harness/client.ts
@@ -739,7 +739,7 @@ namespace ts.server {
             return notImplemented();
         }
 
-        getEncodedSemanticClassifications(_fileName: string, _span: TextSpan, _format?: "original" | "2020"): Classifications {
+        getEncodedSemanticClassifications(_fileName: string, _span: TextSpan, _format?: SemanticClassificationFormat): Classifications {
             return notImplemented();
         }
 
